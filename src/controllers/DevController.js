b2082dfbb70602f6a0983be727c520c370464167
Vinicius Lima <vinicius@loldesign.com.br>
2020-01-14 22:49:24 -0300

    Add validation to check if dev exists before create

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -4,22 +4,27 @@ const Dev = require('../models/Dev')
 module.exports = {
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body
-    const formatedTechs  = techs.split(',').map(tech => tech.trim());
-    const githubResponse = await axios.get(`http://api.github.com/users/${github_username}`)
-    const { name = login, avatar_url, bio } = githubResponse.data
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude]
+
+    let dev = await Dev.findOne({ github_username })
+
+    if(!dev){
+      const formatedTechs  = techs.split(',').map(tech => tech.trim());
+      const githubResponse = await axios.get(`http://api.github.com/users/${github_username}`)
+      const { name = login, avatar_url, bio } = githubResponse.data
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      }
+    
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: formatedTechs,
+        location
+      })
     }
-  
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: formatedTechs,
-      location
-    })
     
     return response.json(dev)
   }
