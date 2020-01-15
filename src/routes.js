ecbd0d38cea03bb9cf2400394009a476c8350fdc
Vinicius Lima <vinicius@loldesign.com.br>
2020-01-14 22:18:20 -0300

    Create route to create users

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,23 @@
-const { Router } = require('express')
-
+const { Router } = require('express');
+const axios = require('axios');
 const routes = Router();
+const Dev = require('./models/Dev')
 
-routes.get('/', (request, response) => {
-  return response.json({name: 'Vinicius'})
+routes.post('/devs', async (request, response) => {
+  const { github_username, techs } = request.body
+  const formatedTechs  = techs.split(',').map(tech => tech.trim());
+  const githubResponse = await axios.get(`http://api.github.com/users/${github_username}`)
+  const { name = login, avatar_url, bio } = githubResponse.data
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: formatedTechs,
+  })
+  
+  return response.json(dev)
 });
 
 module.exports = routes;
