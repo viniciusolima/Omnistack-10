f7c1f60d21e3d0586193b03c0c6fd57d4a37cf23
Vinicius Lima <vinicius@loldesign.com.br>
2020-01-14 22:58:47 -0300

    Create route to list all users

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const routes = Router();
 const DevController = require('./controllers/DevController')
 
 routes.post('/devs', DevController.store);
+routes.get('/devs', DevController.index);
 
 module.exports = routes;
