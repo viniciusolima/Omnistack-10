e47c2a3922aa415cd726d3d1e888283b4b91f714
Vinicius Lima <vinicius@loldesign.com.br>
2020-01-14 21:32:02 -0300

    Create connection with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://root:root@cluster0-hhgta.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.use(express.json());
 
