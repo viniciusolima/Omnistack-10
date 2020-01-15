4b0a8304b8d144da53b9db3b239d79d01ea6c94b
Vinicius Lima <vinicius@loldesign.com.br>
2020-01-14 23:39:11 -0300

    WIP - Creating routes to find user nearby

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 const routes = Router();
 
-const DevController = require('./controllers/DevController')
+const DevController     = require('./controllers/DevController')
+const SearchController = require('./controllers/SearchController')
 
 routes.post('/devs', DevController.store);
 routes.get('/devs', DevController.index);
+
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
